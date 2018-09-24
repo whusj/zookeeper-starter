058c889ea1004d6cdc16b7c13041521e1306ab73
songjiang <whusj@163.com>
2018-09-24 11:42:59 +0800

    0.0.1

---
+++ src/test/java/com/whusj/ZookeeperStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whusj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
